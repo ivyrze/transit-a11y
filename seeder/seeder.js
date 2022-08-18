0f8c7fd7bc96a99eb3b57d1100c9f3e104caccbf
Ivy Rose <hi@ivyrose.me>
2022-08-17 21:20:20 -0400

    Refactor to import agencies synchronously

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -20,64 +20,64 @@ const client = createClient({ url: process.env.REDIS_URL });
 client.on('error', (err) => console.error('Redis client error', err));
 await client.connect();
 
-var cleanPromise = clean(client);
+// Remove existing database data
+await clean(client);
 
-var importPromises = [];
-config.agencies.forEach(agency => {
-    importPromises.push(new Promise(async (resolve) => {
-        let { stops, routes } = await load(agency);
-        
-        stops = stops.map(stop => transformers.idPrefixer(stop, agency.id)) // todo
-        routes = routes.map(route => transformers.colorContinuity(route));
-        
-        // Config-specified transformations
-        if (agency.transformations) {
-            agency.transformations.forEach(transformation => {
-                stops = stops.map(stop => {
-                    return transformers[transformation.type](stop, transformation, 'stop_name');
-                });
+// Import and process GTFS data
+const processAgency = async agency => {
+    let { stops, routes } = await load(agency);
+    
+    stops = stops.map(stop => transformers.idPrefixer(stop, agency.id));
+    routes = routes.map(route => transformers.colorContinuity(route));
+    
+    // Config-specified transformations
+    if (agency.transformations) {
+        agency.transformations.forEach(transformation => {
+            stops = stops.map(stop => {
+                return transformers[transformation.type](stop, transformation, 'stop_name');
             });
-        }
-        
-        // Store stops in Redis database
-        cleanPromise.then(() => {
-            store(client, stops, routes);
-            resolve({ stops, routes });
         });
-    }));
-});
+    }
+    
+    // Store stops in Redis database
+    await store(client, stops, routes);
+    return { stops, routes };
+};
 
-Promise.all(importPromises.concat(cleanPromise)).then((agencies) => {
-    // Create indicies
-    indicies(client).then(() => client.quit());
-    
-    // Respect short-circuit command line option
-    if (args.includes('--skip-geojson')) {
-        console.log("Skipping GeoJSON export and upload.");
-        return;
-    }
-    
-    // Merge the datasets from each agency into one
-    let { stops, routes } = agencies.reduce((result, current) => {
-        Object.keys(current ?? {}).forEach(key => {
-            if (!result[key]) { result[key] = []; }
-            result[key] = result[key].concat(current[key]);
-        });
-        return result;
-    }, {});
-    agencies = undefined;
-    
-    // Convert to GeoJSON and optionally upload to Mapbox
-    const local = args.includes('--export-local');
-    const stopsPromise = geojson('stops', stops, local);
-    const routesPromise = geojson('routes', routes, local);
-    
-    if (!local) {
-        stopsPromise.then(json => mapbox('transit-a11y-stops', json))
-        routesPromise.then(json => mapbox('transit-a11y-routes', json))
-    } else {
-        Promise.all([ stopsPromise, routesPromise ]).then(() => {
-            console.log("Exported GeoJSON to the project root directory.");
-        });
-    }
-});
+let agencies = [];
+for await (let agency of config.agencies) {
+    agencies.push(await processAgency(agency));
+}
+
+// Create indicies
+indicies(client).then(() => client.quit());
+
+// Respect short-circuit command line option
+if (args.includes('--skip-geojson')) {
+    console.log("Skipping GeoJSON export and upload.");
+    process.exit();
+}
+
+// Merge the datasets from each agency into one
+let { stops, routes } = agencies.reduce((result, current) => {
+    Object.keys(current ?? {}).forEach(key => {
+        if (!result[key]) { result[key] = []; }
+        result[key] = result[key].concat(current[key]);
+    });
+    return result;
+}, {});
+agencies = undefined;
+
+// Convert to GeoJSON and optionally upload to Mapbox
+const local = args.includes('--export-local');
+const stopsPromise = geojson('stops', stops, local);
+const routesPromise = geojson('routes', routes, local);
+
+if (!local) {
+    stopsPromise.then(json => mapbox('transit-a11y-stops', json))
+    routesPromise.then(json => mapbox('transit-a11y-routes', json))
+} else {
+    Promise.all([ stopsPromise, routesPromise ]).then(() => {
+        console.log("Exported GeoJSON to the project root directory.");
+    });
+}
