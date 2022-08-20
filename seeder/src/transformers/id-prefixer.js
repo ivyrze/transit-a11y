dd06ae17b7a7841c6e0f1a19618af0f9ddb769fe
Ivy Rose <hi@ivyrose.me>
2022-08-19 20:38:35 -0400

    Handle hyphens in stop IDs

--- seeder/src/transformers/id-prefixer.js
+++ seeder/src/transformers/id-prefixer.js
@@ -1,6 +1,6 @@
 const idPrefixer = (source, id) => {
     if (source.stop_id) {
-        source.stop_id = id + "-" + source.stop_id;
+        source.stop_id = id + "-" + source.stop_id.replace('-', '');
         source.routes = source.routes.map(route => idPrefixer(route, id));
     } else if (source.route_id) {
         source.route_id = id + "-" + source.route_id;
