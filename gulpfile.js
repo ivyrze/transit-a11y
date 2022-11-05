855a1f4df6e8d97e89458f5723c83c06a0239da6
Ivy Rose <hi@ivyrose.me>
2022-11-05 14:44:54 -0400

    Refactored all stylesheets to use SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 import gulp from 'gulp';
+import nodemon from 'gulp-nodemon';
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
 
-const build = () => {
+const sass = gulpSass(dartSass);
+
+export const styles = () => {
+    return gulp.src('./public/scss/style.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./public/css/'));
+};
+
+export const scripts = () => {
     return gulp.src('node_modules/jquery/dist/jquery.min.js')
         .pipe(gulp.dest('public/js/vendor/'));
 };
 
+const build = gulp.series(styles, scripts);
 export default build;
+
+export const watch = () => {
+    return nodemon({
+        script: 'app.js',
+        tasks: [ 'styles' ],
+        ext: 'js json scss pug'
+    });
+};
