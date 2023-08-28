9d6b81529fbe4721f136ad280709408a6a9e45e8
Ivy Rose <hi@ivyrose.me>
2023-08-28 19:59:32 -0400

    Moved main menu into its own component

---
+++ client/src/components/main-menu.jsx
@@ -0,0 +1,86 @@
+import { Link } from 'react-router-dom';
+import { MenuGroup, MenuGroupLabel, MenuItem } from '@ariakit/react';
+import { Menu } from '@components/menu';
+import { useAuth } from '@hooks/auth';
+import { Icon } from '@components/icon';
+
+export const MainMenu = () => {
+    const { auth } = useAuth();
+    
+    return (
+        <div id="main-menu">
+            <Menu iconName="menu">
+                <MenuGroup className="menu-group">
+                    <MenuGroupLabel className="menu-group-label">About</MenuGroupLabel>
+                    <MenuItem render={
+                        <Link
+                            to="/about"
+                            className="menu-item"
+                        >
+                            <Icon name="book" />
+                            About the project
+                        </Link>
+                    } />
+                    <MenuItem render={
+                        <a
+                            href="https://ko-fi.com/ivyrze"
+                            target="_blank"
+                            rel="noopener"
+                            className="menu-item"
+                        >
+                            <Icon name="donation" />
+                            Support us on Ko-fi
+                        </a>
+                    } />
+                </MenuGroup>
+                <MenuGroup className="menu-group">
+                    <MenuGroupLabel className="menu-group-label">View</MenuGroupLabel>
+                    <MenuItem render={
+                        <Link
+                            to="/routes"
+                            className="menu-item"
+                        >
+                            <Icon name="route" />
+                            Show nearby routes
+                        </Link>
+                    } />
+                </MenuGroup>
+                <MenuGroup className="menu-group">
+                    <MenuGroupLabel className="menu-group-label">User</MenuGroupLabel>
+                    { auth && auth.username ? (
+                        <>
+                            <MenuItem render={
+                                <Link
+                                    to={ "/profile/" + auth.username }
+                                    className="menu-item"
+                                >
+                                    <Icon name="user" />
+                                    View profile
+                                </Link>
+                            } />
+                            <MenuItem render={
+                                <Link
+                                    to="/account/logout"
+                                    className="menu-item"
+                                >
+                                    <Icon name="login" />
+                                    Logout
+                                </Link>
+                            } />
+                        </>
+                    ) : (
+                        <MenuItem render={
+                            <Link
+                                to="/account/login"
+                                className="menu-item"
+                            >
+                                <Icon name="login" />
+                                Login
+                            </Link>
+                        } />
+                    ) }
+                </MenuGroup>
+            </Menu>
+        </div>
+    );
+};
--- client/src/pages/(main)/_layout.jsx
+++ client/src/pages/(main)/_layout.jsx
@@ -1,18 +1,14 @@
 import React, { useRef, useCallback } from 'react';
-import { MenuGroup, MenuGroupLabel, MenuItem } from '@ariakit/react';
-import { Menu } from '@components/menu';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Search } from '@components/search';
 import { Map } from '@components/map';
-import { Icon } from '@components/icon';
-import { useAuth } from '@hooks/auth';
+import { MainMenu } from '@components/main-menu';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export const IndexPage = () => {
     const title = 'is the metro accessible?';
     
-    const { auth } = useAuth();
     const map = useRef();
     
     const handleGeolocationTrigger = useCallback(() => {
@@ -25,80 +21,7 @@ export const IndexPage = () => {
                 <h1 className="title">
                     { title }
                 </h1>
-                <div id="main-menu">
-                    <Menu iconName="menu">
-                        <MenuGroup className="menu-group">
-                            <MenuGroupLabel className="menu-group-label">About</MenuGroupLabel>
-                            <MenuItem render={
-                                <Link
-                                    to="/about"
-                                    className="menu-item"
-                                >
-                                    <Icon name="book" />
-                                    About the project
-                                </Link>
-                            } />
-                            <MenuItem render={
-                                <a
-                                    href="https://ko-fi.com/ivyrze"
-                                    target="_blank"
-                                    rel="noopener"
-                                    className="menu-item"
-                                >
-                                    <Icon name="donation" />
-                                    Support us on Ko-fi
-                                </a>
-                            } />
-                        </MenuGroup>
-                        <MenuGroup className="menu-group">
-                            <MenuGroupLabel className="menu-group-label">View</MenuGroupLabel>
-                            <MenuItem render={
-                                <Link
-                                    to="/routes"
-                                    className="menu-item"
-                                >
-                                    <Icon name="route" />
-                                    Show nearby routes
-                                </Link>
-                            } />
-                        </MenuGroup>
-                        <MenuGroup className="menu-group">
-                            <MenuGroupLabel className="menu-group-label">User</MenuGroupLabel>
-                            { auth && auth.username ? (
-                                <>
-                                    <MenuItem render={
-                                        <Link
-                                            to={ "/profile/" + auth.username }
-                                            className="menu-item"
-                                        >
-                                            <Icon name="user" />
-                                            View profile
-                                        </Link>
-                                    } />
-                                    <MenuItem render={
-                                        <Link
-                                            to="/account/logout"
-                                            className="menu-item"
-                                        >
-                                            <Icon name="login" />
-                                            Logout
-                                        </Link>
-                                    } />
-                                </>
-                            ) : (
-                                <MenuItem render={
-                                    <Link
-                                        to="/account/login"
-                                        className="menu-item"
-                                    >
-                                        <Icon name="login" />
-                                        Login
-                                    </Link>
-                                } />
-                            ) }
-                        </MenuGroup>
-                    </Menu>
-                </div>
+                <MainMenu />
                 <Search
                     onGeolocationTriggered={ handleGeolocationTrigger }
                 />
