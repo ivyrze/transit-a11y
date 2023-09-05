6474bd27b38639a2f27a47726322e593287dc13e
Ivy Rose <hi@ivyrose.me>
2023-09-05 13:34:05 -0400

    Workaround for announcements not supporting async

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { wrapRouter } from "oaf-react-router";
+import { RouterWrapper } from '@components/router-wrapper';
 import { routes } from '@generouted/react-router';
 
 import '@assets/styles/style.scss';
 
 const router = createBrowserRouter(routes);
-wrapRouter(router, {
-    documentTitle: () => {
-        const titleComponents = document.title.split(" | ");
-        return titleComponents.length > 1 ? titleComponents[0] : "Home";
-    },
-    navigationMessage: title => "Navigated to " + title,
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <RouterWrapper />
         <RouterProvider router={ router } future={{ v7_startTransition: true }} />
     </React.StrictMode>
 );
