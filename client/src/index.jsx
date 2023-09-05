f3764518b2a44cb8883daf8cdb1b1e91c1462e4b
Ivy Rose <hi@ivyrose.me>
2023-09-05 10:00:09 -0400

    Added ARIA announcements and focus on routing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Routes } from '@generouted/react-router';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { wrapRouter } from "oaf-react-router";
+import { routes } from '@generouted/react-router';
 
 import '@assets/styles/style.scss';
+
+const router = createBrowserRouter(routes);
+wrapRouter(router, {
+    documentTitle: () => {
+        const titleComponents = document.title.split(" | ");
+        return titleComponents.length > 1 ? titleComponents[0] : "Home";
+    },
+    navigationMessage: title => "Navigated to " + title,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Routes />
+        <RouterProvider router={ router } />
     </React.StrictMode>
 );
