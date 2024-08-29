a35bd8b5035e518b1a1c358a926009c87d20d997
Ivy Rose <hi@ivyrose.me>
2024-08-29 15:47:35 -0400

    Fix regression: Vercel analytics

--- src/pages/+layout.svelte
+++ src/pages/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
+    import { inject } from '@vercel/analytics';
+    import { dev } from '$app/environment';
+
     import "$assets/styles/style.scss";
 
     const { children } = $props();
+
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 {@render children()}
