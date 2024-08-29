5005966d62763345230133e9ef33a6ae3e1c4de3
Ivy Rose <hi@ivyrose.me>
2024-08-29 15:44:13 -0400

    Fix regression: Initial map bounds padding

--- src/pages/api/map-bounds/+server.js
+++ src/pages/api/map-bounds/+server.js
@@ -12,6 +12,15 @@ export const GET = async () => {
             bounds: true
         }
     })).bounds;
+
+    const expansionX =
+        (bounds[2] - bounds[0]) * 0.1;
+    const expansionY =
+        (bounds[3] - bounds[1]) * 0.1;
+    bounds[0] -= expansionX;
+    bounds[1] -= expansionY;
+    bounds[2] += expansionX;
+    bounds[3] += expansionY;
     
     return json({ bounds });
 };
