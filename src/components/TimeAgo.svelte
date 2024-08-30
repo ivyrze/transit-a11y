676a2b55b85c880ca6fd9b395c3b084cd92677e9
Ivy Rose <hi@ivyrose.me>
2024-08-30 16:54:04 -0400

    Fix timestamps not updating

--- src/components/TimeAgo.svelte
+++ src/components/TimeAgo.svelte
@@ -7,8 +7,10 @@
         numeric: 'auto'
     });
 
-    const deltaSeconds = (new Date(date) - new Date()) / 1000;
-    const temporalDivisor = [
+    const deltaSeconds = $derived(
+        (new Date(date) - new Date()) / 1000
+    );
+    const temporalDivisor = $derived([
         { unit: 'seconds', amount: 1 },
         { unit: 'minutes', amount: seconds('1m') },
         { unit: 'hours', amount: seconds('1h') },
@@ -18,14 +20,14 @@
         { unit: 'years', amount: seconds('1y') }
     ].reverse().find(division => {
         return division.amount <= Math.abs(deltaSeconds);
-    });
+    }));
 
-    const timeAgo = temporalDivisor ?
+    const timeAgo = $derived(temporalDivisor ?
         formatter.format(
             Math.round(deltaSeconds / temporalDivisor.amount),
             temporalDivisor.unit
         ) :
-        'just now';
+        'just now');
 </script>
 
 <time
